5d6a55305bbaf73d9f03d5d2bbae0247698c9cf1
Mathias Petersen <mathias.petersen95@gmail.com>
2019-01-07 11:47:40 +0100

    add projects

---
+++ Tree/src/App/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+type Tree<'a> = Node of 'a * ('a Tree list)
+
+let movetree (Node((label, x), subtrees), x':float) =
+    Node((label, x+x'), subtrees)
+
+type Extent = (float * float) list
+
+let moveextent (e:Extent, x) = e |> List.map (fun (p, q) -> (p + x, q + x))
+
+let rec merge i =
+    match i with
+    | ([], qs) -> qs
+    | (ps, []) -> ps
+    | ((p,_)::ps, (_,q)::qs) -> (p,q)::merge(ps, qs)
+
+
+[<EntryPoint>]
+let main argv =
+    Library.Say.hello "hej"
+    0 // return an integer exit code
+
