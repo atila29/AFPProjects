0cdbb49d68e85cd59c94c90bd4d295f932e04714
Mathias Petersen <mathias.petersen95@gmail.com>
2019-01-22 12:45:20 +0100

    initial sqlprovider

--- Platform/src/Platform/handlers/handlers.fs
+++ Platform/src/Platform/handlers/handlers.fs
@@ -1,14 +1,31 @@
 module Platform.Handlers
 
+
+open System;
 open Microsoft.AspNetCore.Http
 open FSharp.Control.Tasks.V2.ContextInsensitive
 open Giraffe;
 open Platform.Model
+open FSharp.Data.Sql
+
+
+[<Literal>]
+let connStr = "User ID=postgres;Host=localhost;Port=5432;Database=projectframework;Password=1234"
+
+type sql = SqlDataProvider<Common.DatabaseProviderTypes.POSTGRESQL, connStr>
+
+
+let dbcontext = sql.GetDataContext()
+
 
 let submitRequestHandler: HttpHandler = fun (next : HttpFunc) (ctx : HttpContext) ->
   task {
             // Binds a form payload to a Car object
             let! result = ctx.TryBindFormAsync<Request>()
+            
+            //let projectTable = dbcontext.Public.Project.``Create(desc, teacher, title)``
+
+            let p = dbcontext.Public.Project
 
             return!
                 (match result with
