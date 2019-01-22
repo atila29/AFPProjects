55ca62ba9fc1ffe921ebc0628a712887aff0f056
Mathias Petersen <mathias.petersen95@gmail.com>
2019-01-22 13:50:42 +0100

    fix layout

--- Platform/src/Platform/htmlViews.fs
+++ Platform/src/Platform/htmlViews.fs
@@ -2,6 +2,24 @@ module Platform.HtmlViews
 
 open Giraffe.GiraffeViewEngine
 
+
+let navbar () = ([
+        ul [_class "nav nav-tabs bg-primary"] [
+            li [_class "nav-item"] [
+                a [ _class "nav-link active"; _href "/" ] [ str "index"]
+            ]
+            li [_class "nav-item"] [
+                a [ _class "nav-link active"; _href "/student" ] [ str "student" ]
+            ]
+            li [_class "nav-item"] [
+                a [ _class "nav-link active"; _href "/teacher" ] [ str "teacher" ]
+            ]
+            li [_class "nav-item"] [
+                a [ _class "nav-link active"; _href "/head" ] [ str "head of study" ]
+            ]
+        ]
+    ])
+
 let teacherTemplate () = div[] [
   form [_action "/submitrequest"; _method "post"] [
                 p [] [ encodedText "title" ]
