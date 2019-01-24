7d15007f82aa4779f2942506c7acce00ad8793fe
Nick Kjeldsen <nkjl14@student.sdu.dk>
2019-01-24 13:07:03 +0100

    Seeding and minor input fix

--- Platform/src/Platform/Program.fs
+++ Platform/src/Platform/Program.fs
@@ -9,6 +9,7 @@ open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
 open Platform.Handlers
+open Platform.DBSeed
 
 // ---------------------------------
 // Models
@@ -85,6 +86,7 @@ let configureLogging (builder : ILoggingBuilder) =
 
 [<EntryPoint>]
 let main _ =
+    seedDB |> ignore
     let contentRoot = Directory.GetCurrentDirectory()
     let webRoot     = Path.Combine(contentRoot, "WebRoot")
     WebHostBuilder()
